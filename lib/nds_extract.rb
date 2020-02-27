538fd1f8a22dd2831ccc9c3b8d006ac687511652
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-27 21:31:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-result []
+result {}
 director_data = 0
 result
 end
